a0c69984969f61962ce7394d91c923d583bd1d8e
Rafiki13 <58651523+Rafiki13@users.noreply.github.com>
2021-12-07 15:53:22 +0100

    classe Case

--- src/tests.java
+++ src/tests.java
@@ -1,6 +1,7 @@
 public class tests {
     public static void main(String[] args){
         MEE mee1 = new MEE(26);
+        MEE mee2 = new MEE(new int[] {0, 2, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
 
         System.out.println(mee1.retireAleat());
     }
