6cab6e958709d1131b174c2c962a05ab8b744060
Rafiki13 <58651523+Rafiki13@users.noreply.github.com>
2022-01-09 21:51:22 +0100

    bonus 6

---
+++ SAE-SCRABBLE-DELAYGUES-CAZAUX/bonus6/Case.java
@@ -0,0 +1,56 @@
+public class Case{
+	private int coul;
+	private char lettre; /* ' ' correspond à l'absence de lettre */
+
+	/*
+	* prérequis : uneCouleur est un entier entre 1 et 5
+	* action : constructeur de Case
+	* */
+	public Case (int uneCouleur){
+		this.coul = uneCouleur;
+		this.lettre = ' ';
+	}
+
+	/*
+	* résultat : la couleur de this, un nombre compris entre 1 et 5
+	* */
+	public int getCouleur(){
+		return this.coul;
+	}
+
+	/*
+	* pré-requis : cette case est recouverte
+	* résultat : retourne la lettre sur une case
+	* */
+	public char getLettre(){
+		return this.lettre;
+	}
+
+	/*
+	* pré-requis : let est une lettre majuscule
+	* action : place une lettre sur la case
+	* */
+	public void setLettre(char let){
+		this.lettre = let;
+	}
+
+	/*
+	* résultat : vrai ssi la case est recouverte par une lettre
+	* */
+	public boolean estRecouverte(){
+		return !(this.lettre == ' ');
+	}
+
+	/*
+	* résultat : retourne la couleur de la case ou si elle est recouverte sa lettre
+	* */
+	public String toString(){
+		if(this.estRecouverte()) return String.valueOf(this.lettre);
+
+		if(this.coul > 1){
+			return String.valueOf(this.coul);
+		}else{
+			return " ";
+		}
+	}
+}
